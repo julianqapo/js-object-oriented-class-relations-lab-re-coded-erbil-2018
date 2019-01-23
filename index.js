2f504a48cbaff5c236a5335b596dce5b0f26d947
Julian M. Qapo <julian.qapo@gmail.com>
2019-01-23 20:03:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 
-let store = {drivers:[],passengers: []}
-let idT = 0
-let idD = 0
-let idP = 0
+/*
 class Driver{
 constructor(name,passenger){
 
@@ -53,64 +50,58 @@ class Trip{
 }
 
 
-/*
+*/
+
+
+
 let store = {drivers:[],passengers: []}
+let idT = 0
 let idD = 0
 let idP = 0
 
+
+
+
+
 class Driver{
-  constructor(name,passId){
-    this.name = name;
-    this.idD = ++idD;
-    if(passId){
-      this.passId = passId.idP
-    }
-    store.drivers.push(this)
-  }
-  setPass(passId){
-   this.passId = passId.idP
-    }
-  
- trips(){
-  let x = 0
-for (let i=0;i<store.passengers.length;i++){
-if (this.idD == store.passengers[i].driverId){
+constructor(name, passenger){
+this.idD = idD++
+this.name = name
+if (passenger) {
+    this.userId = passenger.id;
+}
+store.drivers.push(this)
+}
+driven(){
+let x = 0
+for (let i =0;i<store.trips.length;i++){
+if (store.trips[i].userId == this.idD){
 x++
 }
+
 }
-return x
+return x 
 }
-  
-   passengers(){
-let drove = []
-    for (let i=0;i<store.passengers.length;i++){
-      if(this.idD == store.passengers[i].driverId){
-        drove.push(store.passengers[i].pass)
-      }
-    }
-    return drove
-  }
 }
 
-let jojo = new Driver("jojo")
-let ali = new Driver("ali")
 
 
-class Pass{
-  constructor(pass, driver){
-    this.pass = pass
-    this.idP = ++idP
-    if(driver){
-      this.driverId = driver.idD
-    }
-    store.passengers.push(this)
-  }
-  setDeiver(driver){
-    this.driverId = driver.idD
-  }
-}
 
-let sami = new Pass("sami", ali)
-let khalid = new Pass("khalid", ali)
-let joj = new Pass("joj", jojo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
